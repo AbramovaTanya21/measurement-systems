bcf155d1ab55b0433141bf9962de510cb78883d1
AbramovaTanya21 <123319691+AbramovaTanya21@users.noreply.github.com>
2023-03-09 09:56:44 +0300

    Add files via upload

--- Program(Time).vb
+++ Program(Time).vb
@@ -5,22 +5,10 @@ Module Program
 
         Dim cek As Integer
 
-        Dim hour As Integer
-        Dim min As Integer
-
-        Dim cek1 As Integer
-        Dim min1 As Integer
-
         Console.WriteLine("Привет, эты программа может перевести время из секуд в минуты и часы.Хочешь попробывать? Введите время в секундах: ")
         cek = Console.ReadLine()
 
-        hour = cek / 3600
-        min1 = cek / 60
-
-        cek1 = cek Mod 60
-        min = min1 Mod 60
-
-        Console.WriteLine("{0} секунд = время - {1}:{2}:{3}", cek, hour, min, cek1)
+        Console.WriteLine("{0} секунд = время - {1}:{2}:{3}", cek, cek \ 3600, (cek Mod 3600) \ 60, cek Mod 60)
 
     End Sub
 End Module
