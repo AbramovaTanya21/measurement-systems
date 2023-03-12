aecc2c2f8051da7f05b8a63c8948c25b3a06eb73
AbramovaTanya21 <123319691+AbramovaTanya21@users.noreply.github.com>
2023-03-12 13:13:42 +0300

    Add files via upload

---
+++ Program(Length ).vb
@@ -0,0 +1,16 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Console.Write("Хотите перевести число из см. в дц. или м.? Введите число в см: ")
+        Dim cm As Integer = Console.ReadLine()
+
+        'Console.WriteLine("{0}см => {1}м {2}см", cm, cm \ 100, cm Mod 100)
+        'Console.WriteLine("{0}см => {1}дм {2}см ", cm, cm \ 10, cm Mod 10)
+        'Console.WriteLine("{0}см => {1}м {2}дм {3}см", cm, cm \ 100, (cm Mod 100) \ 10, cm Mod 10)
+
+        Console.WriteLine("{0}см => {1}м {4}см => {5}дм {3}см => {1}м {2}дм {3}см", cm, cm \ 100, (cm Mod 100) \ 10, cm Mod 10, cm Mod 100, cm \ 10)
+
+    End Sub
+End Module
