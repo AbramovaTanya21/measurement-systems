0a4a4471fe207400e5e24d37a17faddd9eaf9062
AbramovaTanya21 <123319691+AbramovaTanya21@users.noreply.github.com>
2023-03-16 09:51:38 +0300

    Add files via upload

--- ProgramM1.vb
+++ ProgramM1.vb
@@ -5,9 +5,15 @@ Module ProgramM1
 
         Console.WriteLine("Введите 5 чисел:")
         Dim Inp(4) As Integer
+
         For i = 0 To 4
             Inp(i) = Console.ReadLine()
         Next
+
+
+        Console.WriteLine("Какое число искать?")
+        Dim num As Integer = Console.ReadLine()
+        find(Inp, num)
         'Console.WriteLine(Inp(0))
 
         'Dim Str(4) As Integer
@@ -16,11 +22,9 @@ Module ProgramM1
         'Next
         'Console.WriteLine(Str(0))
 
-        Print(Inp)
+        'Print(Inp)
         'Print(Str)
-        If Inp(0) = 21 Then
-            Console.WriteLine("Первый элемент = 21")
-        End If
+
     End Sub
     Sub BackPrint(List As Integer())
         For i = List.Length - 1 To 0 Step -1
@@ -43,9 +47,16 @@ Module ProgramM1
             Console.WriteLine(List(i))
         Next
     End Sub
-
-
-
+    Function find(List As Integer(), num As Integer) As Integer
+        For i = 0 To List.Length - 1
+            If List(i) = num Then
+                find = 1
+                'Console.WriteLine("{0} элемент = 21", i)
+                Exit Function
+            End If
+        Next
+        find = -1
+    End Function
 
 
 
