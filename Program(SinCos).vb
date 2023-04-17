202f2c1e777fe191c59451950e665c78a6e68b0e
AbramovaTanya21 <123319691+AbramovaTanya21@users.noreply.github.com>
2023-04-17 18:57:42 +0300

    Add files via upload

---
+++ Program(SinCos).vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.Data
+
+Module Program
+    Sub Main(args As String())
+
+        Dim a As Single = 1.5
+        Dim b As Single = 0.7
+        Dim c As Integer = 2
+
+        Dim x As Single = (Math.Exp(a) - Math.Cos(b)) / Math.Sqrt(a * b * c)
+        Console.WriteLine(x)
+
+        Dim y As Single = Math.Log(a) - b * c
+        Console.WriteLine(y)
+
+        Dim z As Single = Math.Sin(x * y) - 2
+        Console.WriteLine(z)
+    End Sub
+End Module
